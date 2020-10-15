3d91bd3270ad802ff3f2f29c2fa1f445c0185cd1
tanmay2601 <tanmay26012000@gmail.com>
2020-10-15 17:43:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require("express");
+var app = express();
+var bodyparser = require("body-parser");
+var mongoose = require("mongoose");
+var Insta = require("instamojo-nodejs");
+
+var API_KEY = "test_f4af45051ca7a9fdc1756caf474";
+
+var AUTH_KEY = "test_9b2e54ff55e2b03d1af3afedfe3";
+
+Insta.setKeys(API_KEY, AUTH_KEY);
+
+Insta.isSandboxMode(true);
+
+app.use(express.static('views'));
+app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    res.render('index.ejs');
+});
+app.get('/form', function (req, res) {
+    res.render('details.ejs');
+})
+app.post('/pay', function (req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var amount = req.body.amount;
+
+    var data = new Insta.PaymentData();
+
+    const REDIRECT_URL = "http://localhost:3000/success";
+
+    data.setRedirectUrl(REDIRECT_URL);
+    data.send_email = "True";
+    data.purpose = "Test"; // REQUIRED
+    data.amount = amount;
+    data.name = name;
+    data.email = email; // REQUIRED
+
+    Insta.createPayment(data, function (error, response) {
+        if (error) {
+            // some error
+            console.log(err);
+        } else {
+            // Payment redirection link at response.payment_request.longurl
+            res.send("Please check your email to make payment");
+        }
+    });
+});
+
+
+app.get('/success',function(req,res){
+    res.send("Please check your email for Payment Invoice if transaction is successfull");
+});
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server has started");
+});
